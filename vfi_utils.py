29db19cc95d785bed4769657d1b9d4c8b8811819
vaguue <43249611+vaguue@users.noreply.github.com>
2026-02-20 23:05:54 +0300

    Add new model download URL to BASE_MODEL_DOWNLOAD_URLS

--- vfi_utils.py
+++ vfi_utils.py
@@ -12,6 +12,7 @@ from comfy.model_management import soft_empty_cache, get_torch_device
 import numpy as np
 
 BASE_MODEL_DOWNLOAD_URLS = [
+    "https://cdn.bookyboo.shop/gopnik/",
     "https://github.com/styler00dollar/VSGAN-tensorrt-docker/releases/download/models/",
     "https://github.com/Fannovel16/ComfyUI-Frame-Interpolation/releases/download/models/",
     "https://github.com/dajes/frame-interpolation-pytorch/releases/download/v1.0.0/"
